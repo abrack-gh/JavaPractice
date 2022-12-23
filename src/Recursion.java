dc640dfffd8bf48f752aa809bf132c1a2b3781d5
Alex Brackenridge <a.brack@live.co.uk>
2022-12-23 14:40:39 +0000

    Recursion

---
+++ src/Recursion.java
@@ -0,0 +1,17 @@
+public class Recursion {
+    public static void main(String[] args) {
+        sayHi(1000); //will call sayHi() 3 times.
+    }
+
+    private static void sayHi(int count){
+        System.out.println("Hi!");
+
+        if(count <= 1 ){
+            return;
+        }
+
+        //Count is x (as defined in main method), so skips above conditional loop.
+
+        sayHi(count - 1); //calls recurisvely with count parameter of 2.
+    }
+}
